131f81bb2d5a4a86309d26ca42ead324ccc4a6e7
faisal-kursheedali <faisal.k.ison@gmail.com>
2022-04-15 14:39:34 +0530

    added Image Component and its Documentation.

--- src/components/doc/import-doc.jsx
+++ src/components/doc/import-doc.jsx
@@ -1,8 +1,9 @@
 // this file import all the default exports of the components,
 // and export them all as named export for this one file
 import Avatar from "./avatar";
+import Image from "./image";
 import Install from "./inistall";
 
 
 
-export {Avatar,Install};
+export {Avatar,Install,Image};
