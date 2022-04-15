7f9ed70a38a0b643c6da68fe991d9a4ea3b5956c
faisal-kursheedali <faisal.k.ison@gmail.com>
2022-04-15 15:27:14 +0530

    added alert component and its documentation.

--- src/components/doc/import-doc.jsx
+++ src/components/doc/import-doc.jsx
@@ -1,8 +1,9 @@
 // this file import all the default exports of the components,
 // and export them all as named export for this one file
+import Alert from "./alert";
 import Avatar from "./avatar";
 import Install from "./inistall";
 
 
 
-export {Avatar,Install};
+export {Avatar,Install,Alert};
